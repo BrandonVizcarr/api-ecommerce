202e669bacae79d6ff92ef204cc74e609db12776
BrandonVizcarr <vizcarrabrandon25@gmail.com>
2025-11-08 17:47:22 -0700

    cambios para categorias

--- src/main/java/com/api_ecommerce/entities/Category.java
+++ src/main/java/com/api_ecommerce/entities/Category.java
@@ -3,6 +3,7 @@ package com.api_ecommerce.entities;
 import java.util.List;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -26,7 +27,7 @@ public class Category {
     private Boolean canceled = false;
     @Column(name = "parent_id")
     private Integer parentId;
-    @OneToMany
-    @JoinColumn(name = "parent_id")
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id", referencedColumnName = "category_id")
     private List<Category> subCategories;
 }
