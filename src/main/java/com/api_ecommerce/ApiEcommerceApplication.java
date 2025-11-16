c03eaba0ca6e020fb53e8eb1fcc46f8ee5198899
BrandonVizcarr <vizcarrabrandon25@gmail.com>
2025-11-15 17:30:38 -0700

    Update ApiEcommerceApplication.java

--- src/main/java/com/api_ecommerce/ApiEcommerceApplication.java
+++ src/main/java/com/api_ecommerce/ApiEcommerceApplication.java
@@ -2,8 +2,11 @@ package com.api_ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
@@ -12,5 +15,20 @@ public class ApiEcommerceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiEcommerceApplication.class, args);
 	}
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200", "https://tudominiofrontend.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
 
 }
