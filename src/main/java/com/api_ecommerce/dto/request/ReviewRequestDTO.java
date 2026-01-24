681c709f980c514d283664f8f790a0c5f876d6d3
BrandonVizcarr <vizcarrabrandon25@gmail.com>
2026-01-24 12:55:20 -0700

    se quita pattern

--- src/main/java/com/api_ecommerce/dto/request/ReviewRequestDTO.java
+++ src/main/java/com/api_ecommerce/dto/request/ReviewRequestDTO.java
@@ -3,7 +3,6 @@ package com.api_ecommerce.dto.request;
 import java.util.List;
 import java.util.UUID;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -11,16 +10,8 @@ public class ReviewRequestDTO {
     @NotNull(message = "desc cannot be null")
     private String desc;
     @NotNull(message = "userId cannot be null")
-    @Pattern(
-    regexp = "^[0-9a-fA-F]{8}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{12}$",
-    message = "UUID must be valid"
-    )
     private UUID userId;
     @NotNull(message = "productId cannot be null")
-    @Pattern(
-    regexp = "^[0-9a-fA-F]{8}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{12}$",
-    message = "UUID must be valid"
-    )
     private UUID productId;
     private List<String> media;
 }
